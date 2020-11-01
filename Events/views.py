cfaa5f7dc1032f63c2614520f2ea733ac6ff7755
Fan4ik20 <61667639+Fan4ik20@users.noreply.github.com>
2020-11-01 02:31:07 +0100

    prealpha

--- Events/views.py
+++ Events/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from .models import Event
+
 # Create your views here.
+
+
+def home(requests):
+    events = Event.objects
+    return render(requests, "Events/home.html", {"events": events})
